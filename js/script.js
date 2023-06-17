bd845446c028cbb16a53e5977bf3da5c4cb6d6ec
WilliamFilh0 <williamfilho8908@gmail.com>
2023-06-17 11:11:53 -0300

    sound and click functions

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const keys = document.querySelectorAll('.key');
+
+const playNote = (note) => {
+  const audio = new Audio('../notes/1.wav');
+  audio.play();
+}
+
+const handleMouseDown = (key) => {
+  playNote();
+
+if(key.className.includes('black')){
+  key.classList.add('black--pressed');
+
+  return;
+}
+
+  key.style.background = '#ddd';
+};
+
+const handleMouseUp = (key) => {
+
+  if(key.className.includes('black')){
+    key.classList.remove('black--pressed');
+    return;
+  }
+
+  key.style.background = 'white';
+};
+
+keys.forEach((key) => {
+  key.addEventListener('mousedown', () => handleMouseDown(key));
+  key.addEventListener('mouseup', () => handleMouseUp(key));
+});
